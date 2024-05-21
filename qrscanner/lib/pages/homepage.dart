4971c708c5cd0b68557b690adb6a68b4f7857aa0
mihpavlovic <127112962+mihpavlovic@users.noreply.github.com>
2024-05-21 15:57:16 +0200

    Finished initial version

--- qrscanner/lib/pages/homepage.dart
+++ qrscanner/lib/pages/homepage.dart
@@ -1,7 +1,10 @@
+import 'dart:typed_data';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:qr_scanner_overlay/qr_scanner_overlay.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,9 +14,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //tokom testiranja staviti na DetectionSpedd.noDuplicates a kada treba da se exportuje staviti na DetectionSpeed.normal
+  MobileScannerController mobScanController = MobileScannerController(detectionSpeed: DetectionSpeed.normal, returnImage: true);
+
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title:  const Center(child: Text("NAOPAKO FEST", style: TextStyle(fontWeight: FontWeight.bold))),
         backgroundColor: Colors.deepOrange,
@@ -44,13 +51,40 @@ class _HomePageState extends State<HomePage> {
 
             Expanded(
               flex: 4,
-              child: MobileScanner(
-                controller: MobileScannerController(
-                  detectionSpeed: DetectionSpeed.noDuplicates
+              
+              child: Stack(
+                children: [
+                  MobileScanner(
+                  controller: mobScanController, //MobileScannerController(
+                    //detectionSpeed: DetectionSpeed.values, // ovo verovatno treba da sklonim
+                    //returnImage: true
+                  //),
+                  
+                  onDetect: (capture){
+                    final List<Barcode> barcodes = capture.barcodes;
+                    final Uint8List? image = capture.image;
+                    mobScanController.stop();
+                    for(final barcode in barcodes){
+                      print('Barcode found! ${barcode.rawValue}');
+                    } 
+                    if(image != null){
+                      showDialog(
+                        context: context, 
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Text("Uspešno očitan QR kod: " + (barcodes.first.rawValue ?? ""), style: const TextStyle(fontSize: 20),),
+                            content: Image(image: MemoryImage(image)),
+                            backgroundColor: Colors.green,
+                            actions: [IconButton(onPressed: (){Navigator.of(context).pop(); mobScanController.start();}, icon: Icon(Icons.close))],
+                          );
+                        }
+                      );
+                    }
+                  }
                 ),
-                onDetect: (capture){
-                  print(capture);
-                }
+                QRScannerOverlay(overlayColor:  Colors.white, borderColor: Colors.deepOrange, scanAreaSize: const Size(300, 450),)
+                
+                ]
               ),
             ),
 
